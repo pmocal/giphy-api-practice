ce93d51a243d94924f265da0b8345d9c2a15ab01
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-08-21 21:26:40 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+async function fetchGIFs(searchTerm) {
+	try {
+		const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=JPATCxkiYzTOAR7667lKYr7ZmxWKQdaD&s='
+			+ searchTerm, {mode: 'cors'})
+		const responseJson = await response.json();
+		document.getElementById('gif').src = responseJson.data.images.original.url;
+	} catch(error) {
+	    console.log('There has been a problem with your fetch operation: ', error.message);
+	};
+}
+
 function main() {
 	if (window.performance) {
 		console.info("window.performance works fine on this browser");
 	}
+
 	if (performance.navigation.type == 1) {
 		console.info( "This page is reloaded" );
 	} else {
@@ -16,15 +28,6 @@ function main() {
 	});
 }
 
-async function fetchGIFs(searchTerm) {
-	try {
-		const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=JPATCxkiYzTOAR7667lKYr7ZmxWKQdaD&s='
-			+ searchTerm, {mode: 'cors'})
-		const responseJson = await response.json();
-		document.getElementById('gif').src = responseJson.data.images.original.url;
-	} catch(error) {
-	    console.log('There has been a problem with your fetch operation: ', error.message);
-	};
-}
+main();
 
-module.exports = { fetchGIFs, main };
+module.exports = { fetchGIFs };
